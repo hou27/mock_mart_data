8bfa897826dd739d3b1ce1c775c8106479366d96
hou27 <ataj125@gmail.com>
2023-08-14 17:57:06 +0900

    이전 사이클의 마지막 시점을 찾는 로직 수정 - #3
    0인 시점을 찾는 것이 아닌 가장 최근 이벤트 시점을 찾도록 수정

--- add_cycle.py
+++ add_cycle.py
@@ -68,23 +68,33 @@ def add_sequence(item_id: int, trend_prob: list, start_date: str):
     return sales_data
 
 
-# 재고량이 0이 된 가장 최근 시점을 찾아내 해당 item_id와 함께 list로 반환하는 method
-def find_zero_stock_date(df):
-    zero_stock_date = []
+# # 재고량이 0이 된 가장 최근 시점을 찾아내 해당 item_id와 함께 list로 반환하는 method
+# def find_zero_stock_date(df):
+#     zero_stock_date = []
+#     for i in df["item_id"].unique():
+#         temp_df = df[df["item_id"] == i]
+#         if temp_df["remaining_stock"].min() == 0:
+#             zero_stock_date.append(
+#                 [i, temp_df[temp_df["remaining_stock"] == 0]["timestamp"].max()]
+#             )
+#     return zero_stock_date
+
+
+# 이전 사이클의 마지막 시점을 찾아내 해당 item_id와 함께 list로 반환하는 method
+def find_last_stock_date(df):
+    last_event_of_prev_cycle = []
     for i in df["item_id"].unique():
         temp_df = df[df["item_id"] == i]
-        if temp_df["remaining_stock"].min() == 0:
-            zero_stock_date.append(
-                [i, temp_df[temp_df["remaining_stock"] == 0]["timestamp"].max()]
-            )
-    return zero_stock_date
+        last_event_of_prev_cycle.append([i, temp_df["timestamp"].max()])
+
+    return last_event_of_prev_cycle
 
 
 # 한 사이클을 더 추가하는 method
 def add_cycle(origin_df: pd.DataFrame, trend_probs: list) -> pd.DataFrame:
     addition_sales_data = []
     # trend_probs = calc_prob(first_cycle_ver6)
-    for item_id, end_date in find_zero_stock_date(origin_df):
+    for item_id, end_date in find_last_stock_date(origin_df):
         addition_sales_data += add_sequence(item_id, trend_probs[item_id], end_date)
 
     addition_sales_df = pd.DataFrame(addition_sales_data)
